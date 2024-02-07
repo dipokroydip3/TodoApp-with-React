e63685d7e6610d0985b074db11fc9ae0b04ecd53
dipokroydip3 <dipokroydip3>
2024-02-07 19:23:56 +0600

    project start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ToDo from './component/ToDo'
+
+function App() {
+  
+
+  return (
+    <div className='bg-[#f7d4fb] w-full h-[100vh]'>
+      <ToDo></ToDo>
+    </div>
+  )
+}
+
+export default App
