94047c8fd957b7e38aed1a439042da888e0cc604
Dipak Roy <dipokroydip3@gmail.com>
2024-02-25 20:23:57 -0800

    some update for text

--- src/component/ToDo.jsx
+++ src/component/ToDo.jsx
@@ -8,7 +8,7 @@ const ToDo = () => {
       
       return (
             <div>
-                  <h1 className="text-4xl font-bold text-center pt-20">TODO WORKS</h1>
+                  <h1 className="text-4xl font-bold text-center pt-20">TODO WORK</h1>
                   <form onSubmit={localStorage} className="flex justify-center my-10" >
                         <input className="px-3 rounded-l-md py-3 w-96" type="text" placeholder="Type here..." />
                         <input className="bg-gray-500 px-3 py-3 rounded-r-md" type="submit" />
